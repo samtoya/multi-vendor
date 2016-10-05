61639f7e8bc4a56578f0a0b5d44fd05cf0185449
samtoya <devcujo3@gmail.com>
2016-10-05 13:45:27 +0000

    Initialize multi-vendor project.

---
+++ include/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="ie8" lang="en"><![endif]-->
+<!--[if IE 9]><html class="ie9" lang="en"><![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+	<meta charset="UTF-8">
+	<title>Retailer | PriceDrummer.com.gh</title>
+	<link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<!-- start: META -->
+	<meta charset="utf-8" />
+	<!--[if IE]><meta http-equiv='X-UA-Compatible' content="IE=edge,IE=9,IE=8,chrome=1" /><![endif]-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta content="" name="description" />
+	<meta content="" name="author" />
+	<!-- end: META -->
+	<!-- start: MAIN CSS -->
+	<link rel="stylesheet" href="assets/css/style.css">
+	<!-- end: CORE CSS -->
+	<link rel="shortcut icon" href="favicon.ico" />
+</head>
+<body>
+	<?php require '../include/navigation.php'; ?>
